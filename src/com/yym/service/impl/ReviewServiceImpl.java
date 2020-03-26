09d266a458d4abb8ec71e036dba8f549fa3979c6
yonney <939964020@qq.com>
2020-03-26 11:38:20 +0800

    实现为返回的复习单词增加选项字段

--- src/com/yym/service/impl/ReviewServiceImpl.java
+++ src/com/yym/service/impl/ReviewServiceImpl.java
@@ -2,6 +2,7 @@ package com.yym.service.impl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,16 @@ public class ReviewServiceImpl implements ReviewService{
 	public List<UserWords> getReviewWords(String table_name, Date dates) {
 		// TODO Auto-generated method stub
 		return reviewDao.getReviewWords(table_name, dates);
+	}
+	@Override
+	public Set<String> getRanExplanation() {
+		// TODO Auto-generated method stub
+		return reviewDao.getRanExplanation();
+	}
+	@Override
+	public Set<String> getRanWord() {
+		// TODO Auto-generated method stub
+		return reviewDao.getRanWord();
 	}
 
 }
