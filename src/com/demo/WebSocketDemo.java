02d6db8a530ea6e0937e74f4c325497016b9c224
yonney <939964020@qq.com>
2020-04-11 13:51:21 +0800

    实现获取房间id号对应的人数

---
+++ src/com/demo/WebSocketDemo.java
@@ -0,0 +1,83 @@
+package com.demo;
+
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.annotation.Resource;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+@ServerEndpoint(value="/newwebsocket/{userId}")
+public class WebSocketDemo {
+	@Resource
+	private WebSocketDemo webSocketDemo;
+	//静态变量，用来记录当前在线连接数，应该把它设计成线程安全的。
+	private static int onlineCount=0;
+	//concurrent包的线程安全set，用来存放每个客户端对应的MyWebSocket对象，若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中key可以为用户标识
+	private static CopyOnWriteArraySet<WebSocketDemo> webSocketSet=new CopyOnWriteArraySet<WebSocketDemo>();
+	//线程安全的Map
+	private static ConcurrentHashMap<String,Session> webSocketMap =new ConcurrentHashMap<String,Session>();
+    /**
+              * 连接开始调用的方法
+    */
+	@OnOpen
+	public void onOpen(Session session,@PathParam("userId")String userId) {
+//		获取从/websocket开始的整条链接，用于获取userId？***=***的参数
+	//	String uri=session.getRequestURI().toString();
+		webSocketSet.add(this);
+		webSocketMap.put(userId, session);
+		addOnlineCount();//在线数加
+		System.out.println(userId+"进入房间");
+		System.out.println("有新连接加入!当前在线人数为"+getOnlineCount());
+	}
+	@OnClose
+	public void onClose(Session session) {
+		System.out.println("close");
+		Map<String,String> map=session.getPathParameters();
+		webSocketMap.remove(map.get("userId"));
+		for(String user:webSocketMap.keySet()) {
+			System.out.print(user);
+		}
+	    subOnlineCount(); //在线数减
+	    System.out.println("有一连接关闭！当前在线人数为" + getOnlineCount());
+	}
+	/*
+	 * 收到客户端消息后调用的方法
+	 */
+	@OnMessage
+	public void onMessage(String message,Session session) throws IOException {
+		System.out.println("来自客户端的消息:"+message);
+		Map<String,String> map=session.getPathParameters();
+		String userId=map.get("userId");
+		System.out.println(webSocketSet);
+		for(String user:webSocketMap.keySet()) {
+			sendMessage(user+"你好，我是"+userId+"  ",webSocketMap.get(user));
+		}
+	}
+	@OnError
+	public void onError(Session session,Throwable error) {
+		System.out.println("发生错误");
+		error.printStackTrace();
+	}
+	public void sendMessage(String message,Session session) {
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message);
+		}
+	}
+	public static synchronized int getOnlineCount() {
+		return onlineCount;
+	}
+	public static synchronized void addOnlineCount() {
+		WebSocketDemo.onlineCount++;
+	}
+	public static synchronized void subOnlineCount() {
+		WebSocketDemo.onlineCount--;
+	}
+}
--- src/com/yym/controller/WebSocketServer.java
+++ src/com/yym/controller/WebSocketServer.java
@@ -1,6 +1,9 @@
 package com.yym.controller;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -11,44 +14,76 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/getServer/{id}")
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.demo.WebSocketDemo;
+
+@ServerEndpoint("/getServer/{roomid}/{uid}")
 public class WebSocketServer {
+	@Autowired
+	private WebSocketServer webSocketServer;
+	//静态变量，用来记录当前在线连接数，应该把它设计成线程安全的。
+	private static int onlineCount=0;
+	//concurrent包的线程安全set，用来存放每个客户端对应的MyWebSocket对象，若要实现服务端与单一客户端通信的话，可以使用Map来存放，其中key可以为用户标识
+	private static CopyOnWriteArraySet<WebSocketDemo> webSocketSet=new CopyOnWriteArraySet<WebSocketDemo>();
+	//线程安全的Map
+	private static ConcurrentHashMap<String,Session> webSocketMap =new ConcurrentHashMap<String,Session>();
+	//用来存放每个房间的人数
+	private static ConcurrentHashMap<String,Integer> webSocketNum=new ConcurrentHashMap<String,Integer>();
+	
 	@OnOpen
-	public void onOpen(Session session,@PathParam("id")int id) {
-		System.out.println("sessionId="+session.getId());
-		System.out.println("id:"+session.getPathParameters().get("id"));
-		//RemoteEndpoint接口是配对的另一端的抽象
-		final RemoteEndpoint.Basic basic=session.getBasicRemote();
-		try {
-			basic.sendText("会话建立成功");
-		}catch(IOException e) {
-			e.printStackTrace();
+	public void onOpen(Session session,@PathParam("roomid")String roomid,@PathParam("uid")String uid) {
+		//将房间号存进map中
+		webSocketMap.put(roomid, session);
+		for(String id:webSocketMap.keySet()) {
+			if(roomid.equals(id)) {	
+				System.out.println(webSocketNum.get("roomid"));
+				if(webSocketNum.get(roomid)!=null) {
+					webSocketNum.put(roomid, webSocketNum.get(roomid)+1);
+				}
+				else if(webSocketNum.get(roomid)==null){
+					webSocketNum.put(roomid,1);
+				}
+				
+			}
 		}
+		System.out.println(webSocketMap);
+		System.out.println(webSocketNum);
+		//this.onMessage(session,"true");
 	}
 
 	/* 收到客户端消息时触发 */
 	@OnMessage
-	public boolean onMessage(Session session,String message) {
-		System.out.println(message);
-		System.out.println("sessionId:"+session.getId());
-		if(message.equals(session.getPathParameters().get("id"))) {
-			return true;
-		}else{
-			return false;
-		}		
+	public String onMessage(Session session,String message) {
+		Map<String,String> map=session.getPathParameters();
+		return message;	
+	}
+	public void sendMessage(String message,Session session) {
+		if(session.isOpen()) {
+			session.getAsyncRemote().sendText(message);
+		}
 	}
 	@OnError
-	public void onError(Throwable throwable,Session session) {
-		System.out.println("sessionId:"+session.getId());
-		System.out.println("pathParams:"+session.getPathParameters());
-		System.out.println("requestParams:"+session.getRequestParameterMap());
-		System.out.println("onError"+throwable.toString());
+	public void onError(Throwable error,Session session) {
+		System.out.println("发生错误");
+		error.printStackTrace();
 	}
 	@OnClose
 	public void onClose(Session session) {
-//		System.out.println("pathParams:"+session.getPathParameters());
-//		System.out.println("requestParams:"+session.getRequestParameterMap());
-		System.out.println("onClose");
+		Map<String,String> map=session.getPathParameters();
+		webSocketMap.remove(map.get("roomid"));
+		webSocketNum.remove(map.get("roomid"));
+		System.out.println(webSocketMap);
+		System.out.println(webSocketNum);
+	}
+	public static synchronized int getOnlineCount() {
+		return onlineCount;
+	}
+	public static synchronized void addOnlineCount() {
+		WebSocketServer.onlineCount++;
+	}
+	public static synchronized void subOnlineCount() {
+		WebSocketServer.onlineCount--;
 	}
 	
 }
